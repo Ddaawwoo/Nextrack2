19948e99004de7f415cc42576ddec6cd76a3a4b2
Dawo <142965685+Ddaawwoo@users.noreply.github.com>
2026-07-17 12:08:04 +0200

    Refresh themes and add Neon Club palette

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dawomix-v11'; // Změň verzi, pokud aktualizuješ soubory
+const CACHE_NAME = 'dawomix-v12'; // Změň verzi, pokud aktualizuješ soubory
 const urlsToCache = [
     './',
     './index.html',
