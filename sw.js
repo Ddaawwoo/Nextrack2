c842d486b9791fecffd7fef2bca20f390590b042
Dawo <terestikdavid@gmail.com>
2026-07-19 08:06:04 +0200

    Add bulk playlist action to library

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dawomix-v24'; // Změň verzi, pokud aktualizuješ soubory
+const CACHE_NAME = 'dawomix-v25'; // Změň verzi, pokud aktualizuješ soubory
 const urlsToCache = [
     './',
     './index.html',
